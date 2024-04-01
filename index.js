19c1495a66bb5c3f411426fa74f27ea534d4f9fe
Tuan Kiet <kietle9a7@gmail.com>
2024-04-02 00:03:15 +0700

    test code

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post(
+  "/api/shorturl",
+  (req, res, next) => {
+    console.log(req.body);
+    next();
+    // dns.lookup(
+    //   "www.t.com",
+    //   { family: 6, all: true },
+    //   (err, address, family) => {
+    //     if (err) {
+    //       res.json({ error: "invalid url" });
+    //       console.log("err: ", err);
+    //     } else {
+    //       console.log("address: %j", address);
+    //       console.log("family: %j", family);
+    //       next();
+    //     }
+    //   }
+    // );
+  },
+  (req, res) => {
+    res.json({ test: "finish" });
+  }
+);
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
