f684d2649a5e5d83ab8da573c41cd19f52f6e65b
Tuan Kiet <kietle9a7@gmail.com>
2024-04-06 03:57:36 +0700

    completed code

--- index.js
+++ index.js
@@ -28,70 +28,71 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-// app.post(
-//   "/api/shorturl",
-//   (req, res, next) => {
-//     dns.lookup(req.body.url, { all: true }, (err, address) => {
-//       if (err) {
-//         console.log(err);
+app.post(
+  "/api/shorturl",
+  // (req, res, next) => {
+  //   console.log(req.body.url);
+  //   const url = new URL(req.body.url);
+  //   console.log(url);
+  //   dns.lookup(url.hostname,(err, address) => {
+  //     if (!address) {
+  //       console.log(err);
+  //       res.json({ error: "invalid url" });
+  //     } else {
+  //       next();
+  //     }
+  //   });
+  // },
+  (req, res) => {
+    const url = new URL(req.body.url);
+    dns.lookup(url.host, async (error, address) => {
+      if (!error) {
+        res.json({ error: "invalid url" });
+      } else {
+        console.log(address);
+        const listRange = await urls.countDocuments();
+        const newUrl = {
+          original_url: url.href,
+          short_url: listRange,
+        };
+        await urls.insertOne(newUrl);
+        res.json({ original_url: req.body.url, short_url: listRange });
+      }
+    });
+  }
+);
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const url = await urls.findOne({ short_url: Number(req.params.short_url) });
+  res.redirect(url.original_url);
+});
+
+// app.post("/api/shorturl", (req, res) => {
+//   const url = req.body.url;
+//   const dnslookup = dns.lookup(
+//     urlparser.parse(url).hostname,
+//     async (err, address) => {
+//       if (!address) {
 //         res.json({ error: "invalid url" });
 //       } else {
-//         next();
+//         const urlCount = await urls.countDocuments();
+//         const urlDoc = {
+//           original_url: url,
+//           short_url: urlCount,
+//         };
+//         const result = await urls.insertOne(urlDoc);
+//         res.json({ original_url: url, short_url: urlCount });
 //       }
-//     });
-//   },
-//   async (req, res) => {
-//     const listRange = await urls.countDocuments();
-//     const newUrl = {
-//       original_url: req.body.url,
-//       short_url: listRange,
-//     };
-//     await urls.insertOne(newUrl);
-//     delete newUrl._id;
-//     res.json(newUrl);
-//   }
-// );
-// app.get("/api/shorturl/:short_url", async (req, res) => {
-//   console.log(+req.params.short_url);
-//   console.log(typeof +req.params.short_url);
-//   // const url = await urls.findOne({ short_url: +req.params.short_url });
-//   // console.log(url);
-//   console.log(url.original_url);
-//   // res.json();
-//   // res.redirect(url.original_url);
-//   res.redirect("https://expressjs.com/en/5x/api.html#res.redirect");
+//     }
+//   );
 // });
 
-app.post("/api/shorturl", (req, res) => {
-  const url = req.body.url;
-  const dnslookup = dns.lookup(
-    urlparser.parse(url).hostname,
-    async (err, address) => {
-      if (!address) {
-        res.json({ error: "invalid " });
-      } else {
-        const urlCount = await urls.countDocuments();
-        const urlDoc = {
-          original_url: url,
-          short_url: urlCount,
-        };
-        const result = await urls.insertOne(urlDoc);
-        res.json({ original_url: url, short_url: urlCount });
-      }
-    }
-  );
-});
-
-app.get("/api/shorturl/:short_url", async (req, res) => {
-  console.log(+req.params.short_url);
-  console.log(typeof +req.params.short_url);
-  const url = await urls.findOne({ short_url: +req.params.short_url });
-  // console.log(url);
-  console.log(url.original_url);
-  // res.json();
-  // res.redirect(url.original_url);
-  res.redirect("https://expressjs.com/en/5x/api.html#res.redirect");
-});
+// app.get("/api/shorturl/:short_url", async (req, res) => {
+//   const url = await urls.findOne({ short_url: +req.params.short_url });
+//   if (!url) {
+//     res.json({ error: "invalid url" });
+//   }
+//   res.redirect(url.original_url);
+// });
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
