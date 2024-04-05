64c9624e6adeb04b01c1d7131f5095c472b80ae0
Tuan Kiet <kietle9a7@gmail.com>
2024-04-06 02:35:07 +0700

    fix code part 4

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const dns = require("dns");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
+const urlparser = require("url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -27,33 +28,71 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.post(
-  "/api/shorturl",
-  (req, res, next) => {
-    dns.lookup(req.body.url, { all: true }, (err, address) => {
-      if (err) {
-        res.json({ error: "invalid url" });
+// app.post(
+//   "/api/shorturl",
+//   (req, res, next) => {
+//     dns.lookup(req.body.url, { all: true }, (err, address) => {
+//       if (err) {
+//         console.log(err);
+//         res.json({ error: "invalid url" });
+//       } else {
+//         next();
+//       }
+//     });
+//   },
+//   async (req, res) => {
+//     const listRange = await urls.countDocuments();
+//     const newUrl = {
+//       original_url: req.body.url,
+//       short_url: listRange,
+//     };
+//     await urls.insertOne(newUrl);
+//     delete newUrl._id;
+//     res.json(newUrl);
+//   }
+// );
+// app.get("/api/shorturl/:short_url", async (req, res) => {
+//   console.log(+req.params.short_url);
+//   console.log(typeof +req.params.short_url);
+//   // const url = await urls.findOne({ short_url: +req.params.short_url });
+//   // console.log(url);
+//   console.log(url.original_url);
+//   // res.json();
+//   // res.redirect(url.original_url);
+//   res.redirect("https://expressjs.com/en/5x/api.html#res.redirect");
+// });
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+  const dnslookup = dns.lookup(
+    urlparser.parse(url).hostname,
+    async (err, address) => {
+      if (!address) {
+        res.json({ error: "invalid " });
       } else {
-        next();
+        const urlCount = await urls.countDocuments();
+        const urlDoc = {
+          original_url: url,
+          short_url: urlCount,
+        };
+        const result = await urls.insertOne(urlDoc);
+        res.json({ original_url: url, short_url: urlCount });
       }
-    });
-  },
-  async (req, res) => {
-    const listRange = await urls.countDocuments();
-    const newUrl = {
-      original_url: req.body.url,
-      short_url: listRange,
-    };
-    await urls.insertOne(newUrl);
-    delete newUrl._id;
-    res.json(newUrl);
-  }
-);
-app.get("api/shorturl/:short_url", async (req, res) => {
-  console.log(req.params.short_url);
-  const url = await urls.findOne({ short_url: +req.params.short_url });
-  res.redirect(url.original_url);
+    }
+  );
 });
+
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  console.log(+req.params.short_url);
+  console.log(typeof +req.params.short_url);
+  const url = await urls.findOne({ short_url: +req.params.short_url });
+  // console.log(url);
+  console.log(url.original_url);
+  // res.json();
+  // res.redirect(url.original_url);
+  res.redirect("https://expressjs.com/en/5x/api.html#res.redirect");
+});
+
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
